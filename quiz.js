8e19c1e810565a2247cf215c27652d6f0f327e43
aminnicevic04 <aminnicevic@gmail.com>
2023-04-25 18:37:06 +0200

    vasilije evo ti hahgshah

--- quiz.js
+++ quiz.js
@@ -1,1 +1,11 @@
-console.log(questions);
+function randomQuestion(num) {
+  for (let i = 0; i < num; i++) {
+    let first = Math.floor(Math.random() * questions.length);
+    let second = Math.floor(Math.random() * questions.length);
+    let something = questions[first];
+    questions[first] = questions[second];
+    questions[second] = something;
+  }
+  console.log(questions);
+}
+randomQuestion(10000);
